055f6fba421c75c2d76ec211531620abcb73cd03
alyonaMol <alen4hukm@gmail.com>
2025-09-24 18:15:54 +0300

    feat: Finish header component

--- pages/components/header_component.py
+++ pages/components/header_component.py
@@ -1,7 +1,28 @@
 """Module for HeaderComponent UI component."""
+from selenium.webdriver.common.by import By
 
 
 class HeaderComponent:
     """Empty HeaderComponent class for header logic."""
+
+    def __init__(self, driver):
+        self.driver = driver
+
+
+        self.menu_link = (By.CSS_SELECTOR, 'a[aria-label="Menu page"]')
+        self.cart_link = (By.CSS_SELECTOR, 'a[aria-label="Cart page"]')
+        self.github_link = (By.CSS_SELECTOR, 'a[aria-label="GitHub page"]')
+
+    def click_menu(self):
+        """Clicks the 'Menu' link."""
+        self.driver.find_element(*self.menu_link).click()
+
+    def click_cart(self):
+        """Clicks the 'Cart' link."""
+        self.driver.find_element(*self.cart_link).click()
+
+    def click_github(self):
+        """Clicks the 'GitHub' link."""
+        self.driver.find_element(*self.github_link).click()
 
     pass
