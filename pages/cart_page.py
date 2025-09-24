247d09f3d1be2859c7d7615c2e9fd41799ffce9b
novitskiiy <veremienko.bogdan@gmail.com>
2025-09-24 19:21:01 +0300

    Implement CartPO in cart_page.py

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -1,12 +1,24 @@
 """Cart page module."""
 
+from typing import List
+
+from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
 
 from pages.base import BasePage
+from pages.components.cart_item_component import CartItemComponent
+from pages.components.pay_component.pay_component import PayComponent
 
 
 class CartPage(BasePage):
     """Cart page object."""
+
+    locators = {
+        "cart_root": (By.CSS_SELECTOR, "main"),
+        "items": (By.CSS_SELECTOR, "ul.cart li"),
+        "pay": (By.CSS_SELECTOR, "section.pay, div.pay, aside.pay"),
+    }
 
     def __init__(self, driver: WebDriver) -> None:
         """
@@ -16,3 +28,19 @@ class CartPage(BasePage):
             driver: Selenium WebDriver instance.
         """
         super().__init__(driver)
+
+    def _root(self) -> WebElement:
+        """Return the root container for the cart page content."""
+        return self.find_element(self.locators["cart_root"])
+
+    def items(self) -> List[CartItemComponent]:
+        """Return list of cart item components."""
+        root = self._root()
+        elements = root.find_elements(*self.locators["items"])  # type: ignore[arg-type]
+        return [CartItemComponent(self.driver, el) for el in elements]
+
+    def pay(self) -> PayComponent:
+        """Return the pay component for the cart page."""
+        root = self._root()
+        pay_element = root.find_element(*self.locators["pay"])  # type: ignore[arg-type]
+        return PayComponent(self.driver, pay_element)  # type: ignore[arg-type]
