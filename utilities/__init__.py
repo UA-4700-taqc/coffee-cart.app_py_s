f98d1771e8cb5b4569b1a5bbc0afb08753a57449
66redBastard <66redbastard@gmail.com>
2025-10-02 10:00:26 +0300

    add util to get styles

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -1,5 +1,6 @@
 """Set Utilities package for test automation project."""
 
 from .logger import Logger, get_logger
+from .styles_getter import StylesGetter
 
-__all__ = ["Logger", "get_logger"]
+__all__ = ["Logger", "get_logger", "StylesGetter"]
