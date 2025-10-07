57632f087f00cab0c48eee1f71cb9e5412ef120e
Lemekh Taras <lemekhtaras2@gmail.com>
2025-10-07 12:49:44 +0300

    update pages | add test_purchase.py

--- pages/menu_page.py
+++ pages/menu_page.py
@@ -1,11 +1,14 @@
 """Menu page for coffee items."""
 from typing import List, Optional
 
+from selenium.common import NoSuchElementException, TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
+from config.resources import IMPLICIT_WAIT
 from pages.base import BasePage, DictLocatorType
 from pages.components.cup_component.cup_component import CupComponent
 from pages.components.promo_component import PromoComponent
@@ -14,7 +17,13 @@ from pages.components.promo_component import PromoComponent
 class MenuPage(BasePage):
     """Coffee menu page."""
 
-    locators: DictLocatorType = {"cups": (By.XPATH, "//li/h4/.."), "promo": (By.CLASS_NAME, "promo")}
+    locators: DictLocatorType = {
+        "cups": (By.XPATH, "//li/h4/.."),
+        "promo": (By.CLASS_NAME, "promo"),
+        "pay_button": (By.CSS_SELECTOR, "button.pay"),
+        "pay_modal": (By.CSS_SELECTOR, "div.modal"),
+        "success_snackbar": (By.CSS_SELECTOR, "div.snackbar"),
+    }
 
     def __init__(self, driver: WebDriver) -> None:
         """
@@ -80,3 +89,34 @@ class MenuPage(BasePage):
         """
         promo = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(self.locators["promo"]))
         return PromoComponent(self.driver, promo)
+
+    def click_pay_button(self) -> "PaymentDetailsModal":  # noqa=F821
+        """
+        Click on pay button.
+
+        Returns:
+            Instance of PaymentDetailsModal.
+        """
+        from pages.components.payment_details_modal import PaymentDetailsModal
+
+        self.find_element(self.locators["pay_button"]).click()
+        pay_modal_we = self.find_element(self.locators["pay_modal"])
+        return PaymentDetailsModal(self.driver, pay_modal_we)
+
+    def get_snackbar_success_we(self) -> WebElement | None:
+        """
+        Wait until the success snack bar becomes visible (style='') and return it.
+
+        Returns:
+            WebElement | None: if not visible.
+        """
+        try:
+            self.driver.implicitly_wait(0)
+            WebDriverWait(self.driver, 5).until(
+                lambda wd: (wd.find_element(*self.locators["success_snackbar"]).get_attribute("style") or "") == ""
+            )
+            return self.find_element(self.locators["success_snackbar"])
+        except (NoSuchElementException, TimeoutException):
+            return None
+        finally:
+            self.driver.implicitly_wait(IMPLICIT_WAIT)
